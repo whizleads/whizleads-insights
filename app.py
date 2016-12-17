e1d56df54ccf5c106d498a3429b1d913468c3b4b
Javier Camarasa <launch@whizleads.com>
2016-12-17 18:48:25 +1100

    GET /insight endpoint.

--- app.py
+++ app.py
@@ -12,24 +12,33 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/lead', methods=['GET', 'POST'])
+@app.route('/lead')
 def lead():
 
-	if request.method == 'POST':
+	connection = httplib.HTTPSConnection('parseapi.back4app.com', 443)
+	connection.connect()
+	connection.request('GET', '/classes/Lead', '', {
+	       "X-Parse-Application-Id": "9LT6MCUSdT4mnzlNkG2pS8L51wvMWvugurQJnjwB",
+	       "X-Parse-REST-API-Key": "6gwEVURQBIkh9prcc3Bgy8tRiJTFYFbJJkQsB45w"
+	     })
+	result = json.loads(connection.getresponse().read())
+	print result
 
+	return jsonify(result)
 
-		return "POST"
-    else:
-		connection = httplib.HTTPSConnection('parseapi.back4app.com', 443)
-		connection.connect()
-		connection.request('GET', '/classes/Lead', '', {
-		       "X-Parse-Application-Id": "9LT6MCUSdT4mnzlNkG2pS8L51wvMWvugurQJnjwB",
-		       "X-Parse-REST-API-Key": "6gwEVURQBIkh9prcc3Bgy8tRiJTFYFbJJkQsB45w"
-		     })
-		result = json.loads(connection.getresponse().read())
-		print result
+@app.route('/Insight')
+def insight():
 
-		return jsonify(result)
+	connection = httplib.HTTPSConnection('parseapi.back4app.com', 443)
+	connection.connect()
+	connection.request('GET', '/classes/Insight', '', {
+	       "X-Parse-Application-Id": "9LT6MCUSdT4mnzlNkG2pS8L51wvMWvugurQJnjwB",
+	       "X-Parse-REST-API-Key": "6gwEVURQBIkh9prcc3Bgy8tRiJTFYFbJJkQsB45w"
+	     })
+	result = json.loads(connection.getresponse().read())
+	print result
+
+	return jsonify(result)
 
 port = int(os.environ.get('PORT', 5000))
 app.run(host="0.0.0.0", port=port, debug=True)
