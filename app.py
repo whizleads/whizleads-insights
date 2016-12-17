1bebfeba1e9c5227fb85f7a6f9ad01beaa825b2b
Javier Camarasa <launch@whizleads.com>
2016-12-17 18:36:10 +1100

    Added POST structure.

--- app.py
+++ app.py
@@ -12,19 +12,24 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/lead')
+@app.route('/lead', methods=['GET', 'POST'])
 def lead():
 
-	connection = httplib.HTTPSConnection('parseapi.back4app.com', 443)
-	connection.connect()
-	connection.request('GET', '/classes/Lead', '', {
-	       "X-Parse-Application-Id": "9LT6MCUSdT4mnzlNkG2pS8L51wvMWvugurQJnjwB",
-	       "X-Parse-REST-API-Key": "6gwEVURQBIkh9prcc3Bgy8tRiJTFYFbJJkQsB45w"
-	     })
-	result = json.loads(connection.getresponse().read())
-	print result
+	if request.method == 'POST':
 
-	return jsonify(result)
+
+		return "POST"
+    else:
+		connection = httplib.HTTPSConnection('parseapi.back4app.com', 443)
+		connection.connect()
+		connection.request('GET', '/classes/Lead', '', {
+		       "X-Parse-Application-Id": "9LT6MCUSdT4mnzlNkG2pS8L51wvMWvugurQJnjwB",
+		       "X-Parse-REST-API-Key": "6gwEVURQBIkh9prcc3Bgy8tRiJTFYFbJJkQsB45w"
+		     })
+		result = json.loads(connection.getresponse().read())
+		print result
+
+		return jsonify(result)
 
 port = int(os.environ.get('PORT', 5000))
 app.run(host="0.0.0.0", port=port, debug=True)
