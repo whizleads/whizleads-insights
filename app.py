a8f01f0a41b9f02ab26997715326fbb07fd571fc
Javier Camarasa <launch@whizleads.com>
2016-12-17 18:20:25 +1100

    Added return response.

--- app.py
+++ app.py
@@ -24,8 +24,12 @@ def lead():
 	result = json.loads(connection.getresponse().read())
 	print result
 
+	return jsonify({'Successfully GET data from Lead Class.'})
+
+
 port = int(os.environ.get('PORT', 5000))
 app.run(host="0.0.0.0", port=port, debug=True)
+
 
 if __name__ == "__main__":
     app.run()
